e8f3636229ce7b318ea95a64e4ecb71a3f10a9d7
lx kish <urban.ktulhu@gmail.com>
2018-09-12 00:06:36 +1200

    177 CSS Loaders

--- project/index.js
+++ project/index.js
@@ -1,3 +1,5 @@
 import data from './users';
+import './css/style.css';
+import './css/user.css';
 
 console.log(`My name is ${data.name}, I'm ${data.age} years old.`);
--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,26 @@ module.exports = {
     entry: './project/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: 'build/'
     },
     module: {
         rules: [
             {
                 use: 'babel-loader',
-                test:  /\.js$/
+                test: /\.js$/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader/url" },
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
             }
         ]
     }
